ec5181f7f4e62c3beeb0b2335fad1775f0c00aa3
Jim Kang <jimkang@gmail.com>
2017-03-24 17:23:26 -0400

    Strip newlines from base64 to be decoded so there's no problems on Mobile Safari.

--- safe-encoders.js
+++ safe-encoders.js
@@ -1,3 +1,5 @@
+var newlineRegex = /\n/g;
+
 function b64EncodeUnicode(str) {
   return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
     return String.fromCharCode('0x' + p1);
@@ -5,9 +7,19 @@ function b64EncodeUnicode(str) {
 }
 
 function b64DecodeUnicode(str) {
-  return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  // atob on Mobile Safari for iOS 9 will throw an exception if there's a newline.
+  var b64Decoded = atob(str.replace(newlineRegex, ''));
+  var decodedWithUnicodeHexesRestored = Array.prototype.map.call(
+    b64Decoded,
+    hexEncodeCharCode
+  )
+  .join('');
+
+  return decodeURIComponent(decodedWithUnicodeHexesRestored);
+}
+
+function hexEncodeCharCode(c) {
+  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
 }
 
 module.exports = {
