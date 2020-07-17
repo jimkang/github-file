ea6a91859424933a9eb8be24187feccab411366f
Jim Kang <jimkang@gmail.com>
2020-07-16 23:25:10 -0400

    Make tests run sequentially (create, then get, then update) so that the get and update with SHA tests have something to get and update. Unsure the tests worked before.

--- tests/github-file-tests.js
+++ tests/github-file-tests.js
@@ -5,20 +5,20 @@ var assertNoError = require('assert-no-error');
 var GitHubFile = require('../index');
 var config = require('../test-config');
 var cloneDeep = require('lodash.clonedeep');
+var { queue } = require('d3-queue');
 
 var request;
 
 if (typeof window === 'object') {
   // Don't use the Node request module for the browser test!
   request = require('basic-browser-request');
-}
-else {
+} else {
   request = require('request');
 }
 
 var encodeInBase64;
 var decodeFromBase64;
- 
+
 if (typeof window !== 'object' || !window.btoa) {
   encodeInBase64 = function encodeFromBase64(s) {
     return Buffer.from(s, 'utf8').toString('base64');
@@ -46,20 +46,27 @@ var testCases = [
     content: 'Hello, here is some content: ' + randomId(20)
   },
   {
-    filePath: 'directory-b/subdirectory-' + randomId(5) + '/' + randomId(14) + '.txt',
+    filePath:
+      'directory-b/subdirectory-' + randomId(5) + '/' + randomId(14) + '.txt',
     content: 'Hello, here is some content: ' + randomId(20)
   },
   {
     filePath: 'directory-a/' + randomId(14) + '.txt',
     content: 'Hello, here is some content: ' + randomId(20)
-  },
+  }
 ];
 
-testCases.forEach(runUpdateTest);
-testCases.forEach(runGetTest);
-testCases.forEach(runUpdateWithSHATest);
+var q = queue(1);
+testCases.forEach(queueTests);
+q.awaitAll(conclude);
 
-function runGetTest(testCase) {
+function queueTests(testCase) {
+  q.defer(runUpdateTest, testCase);
+  q.defer(runGetTest, testCase);
+  q.defer(runUpdateWithSHATest, testCase);
+}
+
+function runGetTest(testCase, done) {
   test('Getting ' + testCase.filePath, getFileTest);
 
   function getFileTest(t) {
@@ -70,13 +77,16 @@ function runGetTest(testCase) {
       assertNoError(t.ok, error, 'No error from getFile.');
       t.ok(result.sha, 'Result has a SHA.');
       t.equal(result.content, testCase.content, 'Content is correct.');
-      console.log('Please head over to https://github.com/jimkang/github-file-test/commits/ and manually inspect the commits created by these tests.');
+      console.log(
+        'Please head over to https://github.com/jimkang/github-file-test/commits/ and manually inspect the commits created by these tests.'
+      );
       t.end();
+      done();
     }
   }
 }
 
-function runUpdateTest(testCase) {
+function runUpdateTest(testCase, done) {
   test('Updating ' + testCase.filePath, updateTest);
 
   function updateTest(t) {
@@ -87,11 +97,12 @@ function runUpdateTest(testCase) {
       assertNoError(t.ok, error, 'No error from updateFile.');
       t.ok(commit.sha, 'sha is passed.');
       t.end();
+      done();
     }
   }
 }
 
-function runUpdateWithSHATest(testCase) {
+function runUpdateWithSHATest(testCase, done) {
   test('Getting ' + testCase.filePath, updateWithSHATest);
 
   function updateWithSHATest(t) {
@@ -111,6 +122,15 @@ function runUpdateWithSHATest(testCase) {
       assertNoError(t.ok, error, 'No error from updateFile.');
       t.ok(content.sha, 'sha is passed.');
       t.end();
+      done();
     }
+  }
+}
+
+function conclude(error) {
+  if (error) {
+    console.log('Concluded with error:', error);
+  } else {
+    console.log('Tests concluded.');
   }
 }
